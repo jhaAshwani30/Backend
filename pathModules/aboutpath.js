45e97af1e8361d692de16c4c894237059e64dd2c
Ashwani jha <129526274+jhaAshwani30@users.noreply.github.com>
2024-08-15 22:27:08 +0530

    Create aboutpath.js

---
+++ pathModules/aboutpath.js
@@ -0,0 +1,9 @@
+import path from "path"
+
+let myPath = "C:\\Users\\iitia\\Downloads\\ Web Development Course\\messi\\harry.txt"
+console.log(path.extname(myPath))
+
+console.log(path.dirname(myPath))
+console.log(path.basename(myPath))
+
+console.log(path.join("c:/", "programs\\harry.txt"))
